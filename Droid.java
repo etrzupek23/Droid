e7a95e68852212396c970c79f165dfcf5a7e7d1c
etrzupek23 <57397879+etrzupek23@users.noreply.github.com>
2020-02-14 13:46:56 -0500

    Add files via upload

---
+++ Droid.java
@@ -0,0 +1,40 @@
+public class Droid {
+  //instances
+  int batteryLevel;
+  String name;
+  //constructor
+  public Droid(String droidName){
+    name = droidName;
+    batteryLevel = 100;
+  }
+  //prints out the droid's introduction sentance
+  public String toString(){
+    return "Hello, I’m the droid " + name + ".";
+  }
+  //prints out what a certain droid is doing
+  public void performTask(String task){
+    System.out.println(name + " is now " + task + ".");
+    batteryLevel = batteryLevel - 10;
+  }
+  public void energy(){
+    int battery = batteryLevel;
+    System.out.println(name +"'s battery level is now at " + batteryLevel + ".");
+  }
+  //the main method
+  public static void main(String [] args){
+    Droid Marissa = new Droid("Marissa");
+    Droid Amanda = new Droid("Amanda");
+  //all of Marissa's code
+    System.out.println(Marissa);
+    Marissa.performTask("dancing");
+    Marissa.energy();
+    Marissa.performTask("studying");
+    Marissa.energy();
+  //space
+    System.out.println("");
+  //all of Amanda's code
+    System.out.println(Amanda);
+    Amanda.performTask("playing basketball");
+    Amanda.energy();
+  }
+}
